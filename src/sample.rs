588173a6694640404493ccc127d2656c844bbfea
Robert Cobb <rwcobbjr@gmail.com>
2020-07-06 12:27:37 -0400

    smartstrings use even fewer allocations

--- src/sample.rs
+++ src/sample.rs
@@ -1,6 +1,56 @@
 use argh::FromArgs;
 use parse_display::{Display, FromStr};
 
+use smartstring::{LazyCompact, SmartString};
+struct SmartWrap(SmartString<LazyCompact>);
+
+impl From<String> for SmartWrap {
+    fn from(s: String) -> Self {
+        Self(s.into())
+    }
+}
+
+impl From<&str> for SmartWrap {
+    fn from(s: &str) -> Self {
+        Self(s.into())
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for SmartWrap {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use ::serde::de::{Error, Visitor};
+        use std::fmt;
+
+        struct SmartVisitor;
+
+        impl<'a> Visitor<'a> for SmartVisitor {
+            type Value = SmartWrap;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a string")
+            }
+
+            fn visit_str<E: Error>(self, v: &str) -> Result<Self::Value, E> {
+                Ok(v.into())
+            }
+
+            fn visit_borrowed_str<E: Error>(self, v: &'a str) -> Result<Self::Value, E> {
+                Ok(v.into())
+            }
+
+            fn visit_string<E: Error>(self, v: String) -> Result<Self::Value, E> {
+                Ok(v.into())
+            }
+        }
+
+        deserializer.deserialize_str(SmartVisitor)
+    }
+}
+
+
 #[derive(FromArgs)]
 /// Run sample code
 #[argh(subcommand, name = "sample")]
@@ -23,7 +73,7 @@ impl Sample {
         match self.lib {
             Lib::Std => self.read_records::<String>(),
             Lib::Smol => self.read_records::<smol_str::SmolStr>(),
-            Lib::Smart => todo!(),
+            Lib::Smart => self.read_records::<SmartWrap>(),
         }
     }
 
